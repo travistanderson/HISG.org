52d5f2bc4036414e47576acf1c059b625cac1ba1
Travis Anderson <tanderson@hisg.org>
2009-07-20 11:17:38 -0400

    pages has fk to bricks now and works

--- projects/complete_project/apps/pages/models.py
+++ projects/complete_project/apps/pages/models.py
@@ -1,6 +1,6 @@
 # pages/models.py
 from django.db import models
-# from brick.models import Webpage
+from brick.models import Webpage
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,12 +8,12 @@ class Page(models.Model):
 	url = models.CharField(max_length=100, db_index=True)
 	title = models.CharField(max_length=200)
 	content = models.TextField(blank=True)
-	enable_comments = models.BooleanField(_('enable comments'))
+	# enable_comments = models.BooleanField(_('enable comments'))
 	templatr = models.CharField(max_length=70, blank=True,)
 	active = models.BooleanField(default=True)
-	registration_required = models.BooleanField(_('registration required'), help_text=_("If this is checked, only logged-in users will be able to view the page."))
-	sites = models.ManyToManyField(Site)
-	# bricks = models.ForeignKey(Webpage, blank=True, null=True)
+	# registration_required = models.BooleanField(_('registration required'), help_text=_("If this is checked, only logged-in users will be able to view the page."))
+	# sites = models.ManyToManyField(Site)
+	bricks = models.ForeignKey(Webpage, blank=True, null=True)
 
 	class Meta:
 		ordering = ('url',)
--- projects/complete_project/apps/pages/views.py
+++ projects/complete_project/apps/pages/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.core.xheaders import populate_xheaders
 from django.utils.safestring import mark_safe
+from brick.models import Webpage
 
 DEFAULT_TEMPLATE = 'about/history.html'
 
@@ -24,12 +25,13 @@ def pager(request, url):
         return HttpResponseRedirect("%s/" % request.path)
     if not url.startswith('/'):
         url = "/" + url
-    f = get_object_or_404(Page, url__exact=url, sites__id__exact=settings.SITE_ID)
+    f = get_object_or_404(Page, url__exact=url)
+    p = get_object_or_404(Webpage, name=f.bricks.name)
     # If registration is required for accessing this page, and the user isn't
     # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated():
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(request.path)
+    # if f.registration_required and not request.user.is_authenticated():
+    #     from django.contrib.auth.views import redirect_to_login
+    #     return redirect_to_login(request.path)
     if f.templatr:
         t = loader.select_template((f.templatr, DEFAULT_TEMPLATE))
     else:
@@ -42,11 +44,10 @@ def pager(request, url):
     f.content = mark_safe(f.content)
 
     c = RequestContext(request, {
-        'page': f,
+        'pager': f,
+		'page': p,
     })
     response = HttpResponse(t.render(c))
     populate_xheaders(request, response, Page, f.id)
     return response
-
-
 

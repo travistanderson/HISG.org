671c6cb5c22ec2406f7f21dcffc092ed73aeee88
Travis Anderson <tanderson@hisg.org>
2009-11-25 16:11:59 -0500

    changed default sorting on projects index page to 'by-date'

--- projects/complete_project/apps/projectsmodels/urls.py
+++ projects/complete_project/apps/projectsmodels/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'projectsmodels.views.index', name='projectsmodels'),
-	url(r'^projects/$', 'projectsmodels.views.projectindex', {'sort': 'country'}, name='project'),
-	url(r'^projects/sort-by-date/$', 'projectsmodels.views.projectindex', {'sort': 'date'}, name='project_date'),
+	url(r'^projects/$', 'projectsmodels.views.projectindex', {'sort': 'date'}, name='project'),
+	url(r'^projects/sort-by-country/$', 'projectsmodels.views.projectindex', {'sort': 'country'}, name='project_country'),
 	url(r'^projects/sort-by-region/$', 'projectsmodels.views.projectindex', {'sort': 'region'}, name='project_region'),	
 	url(r'^projects/(?P<proj_id>\d+)/$', 'projectsmodels.views.projectdetail', name='project_detail'),
 	

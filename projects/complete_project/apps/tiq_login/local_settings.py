2872d3ceb41db50ce627f0ca2c70d6a70e930e86
django <django@django2.dfw.hisg-it.net.dfw.hisg-it.net>
2009-12-08 12:46:35 -0600

    removed tiq local settings

--- projects/complete_project/apps/tiq_login/local_settings.py
+++
@@ -1,5 +0,0 @@
-# This is the location where you store your local copy of tiqLibraries. It should be an absolute path, use forward slashes, and end with a trailing slash
-SYS_PATH_TO_TIQ_LIBRARIES = '/Users/travis/Dropbox/dev/python'
-
-# The Hostname and Path of the SessionRPC Server
-TIQ_SERVER = '192.168.1.104' # hostname

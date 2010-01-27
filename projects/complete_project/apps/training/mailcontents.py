6eac81855bbbf3f4ba39bf8b5236b4475cb6bbbc
Travis Anderson <tanderson@hisg.org>
2010-01-27 18:13:00 -0500

    updated mail message again

--- projects/complete_project/apps/training/mailcontents.py
+++ projects/complete_project/apps/training/mailcontents.py
@@ -8,15 +8,26 @@ def sumail(event,user):
 	SU_TOEMAIL = [str(user.email),"cjennings@hisg.org",str(event.contact.email)]
 	SU_SUBJECT = "HISG signup confirmation email for "+str(event)+"."
 	# SU_CONTENT = "Dear "+str(user.first_name)+",\n\nThank you for registering to attend "+str(event)+". It will begin on "+event.start_date.strftime("%b. %d, %Y")+" and end on "+event.end_date.strftime("%b. %d, %Y")+". As the event approaches, you will receive a follow-up email confirming last minute details."+str(event.contact)+" is the contact person for this training event, please contact him/her at "+str(event.contact.email)+" with any questions you may have. Thank you for signing up. We will see you there. \n\Sincerely,\nHISG Training Staff."
+	# event.start_date.strftime("%b. %d"),event.end_date.strftime("%d, %Y")
+	start_date = event.start_date
+	end_date = event.end_date
+	if start_date == end_date:
+		thedate = '''on %s''' %(start_date.strftime("%b. %d, %Y"))
+	else:
+		if start_date.month == end_date.month:
+			thedate = '''from %s-%s''' %(start_date.strftime("%b. %d"),event.end_date.strftime("%d, %Y"))
+		else:
+			thedate = '''from %s-%s''' %(start_date.strftime("%b. %d"),event.end_date.strftime("%b. %d, %Y"))
+		
 	SU_CONTENT = '''Dear %s,
 	
-	Thank you for registering to attend the training in %s from %s - %s. 
+	Thank you for registering to attend the training in %s %s. 
 As the event approaches, you will receive a follow-up email confirming final details. %s (%s) or Charlene Jennnings (cjennings@hisg.org) are the contact people for this training event. Please contact them should you have any questions.
 
 Thank you for signing up. We look forward to seeing you soon!
 
 	
-HISG Training Staff	''' %(str(user.first_name),str(event),event.start_date.strftime("%b. %d"),event.end_date.strftime("%d, %Y"),str(event.contact),str(event.contact.email))
+HISG Training Staff	''' %(str(user.first_name),str(event.location),thedate,str(event.contact),str(event.contact.email))
 
 	return SU_SUBJECT, SU_CONTENT, SU_FROMEMAIL, SU_TOEMAIL
 	

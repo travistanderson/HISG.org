2afbc94525de5afdf542a660209b773dadb8026f
Travis Anderson <tanderson@hisg.org>
2010-05-20 17:15:05 -0400

    added steve to recipient list for registration email

--- projects/complete_project/apps/training/mailcontents.py
+++ projects/complete_project/apps/training/mailcontents.py
@@ -5,7 +5,7 @@ from training.models import EmailTemplate, Event
 # here are the defaults for a success confirmation email after someone signs up
 def sumail(event,user):
 	SU_FROMEMAIL = str(event.contact.email)
-	SU_TOEMAIL = [str(user.email),"cjennings@hisg.org",str(event.contact.email)]
+	SU_TOEMAIL = [str(user.email),"cjennings@hisg.org","smix@hisg.org",str(event.contact.email)]
 	SU_SUBJECT = "HISG signup confirmation email for "+str(event)+"."
 	# SU_CONTENT = "Dear "+str(user.first_name)+",\n\nThank you for registering to attend "+str(event)+". It will begin on "+event.start_date.strftime("%b. %d, %Y")+" and end on "+event.end_date.strftime("%b. %d, %Y")+". As the event approaches, you will receive a follow-up email confirming last minute details."+str(event.contact)+" is the contact person for this training event, please contact him/her at "+str(event.contact.email)+" with any questions you may have. Thank you for signing up. We will see you there. \n\Sincerely,\nHISG Training Staff."
 	# event.start_date.strftime("%b. %d"),event.end_date.strftime("%d, %Y")

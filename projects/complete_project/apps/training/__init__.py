de1b6d58dc8d703af1976453d5f8e42336ff89e1
Travis Anderson <tanderson@hisg.org>
2011-02-10 17:40:08 -0500

    one more rpc change

--- projects/complete_project/apps/training/__init__.py
+++ projects/complete_project/apps/training/__init__.py
@@ -4,6 +4,30 @@ from django.db.models import Q
 from rpc4django import rpcmethod
 from training.models import Event
 
-@rpcmethod(name='login.add',)
+@rpcmethod(name='training.add',)
 def add(a,b):
 	return a+b
+	
+	
+@rpcmethod(name='training.events',)
+def events():
+	e = Event.objects.all()
+	events = []
+	for event in e:
+		theevent = {}
+		theevent['name'] = event.name
+		theevent['contact'] = event.contact
+		theevent['subject'] = event.subject
+		theevent['start_date'] = event.start_date
+		theevent['end_date'] = event.end_date
+		theevent['location'] = event.location
+		theevent['latitude'] = event.latitude
+		theevent['longitude'] = event.longitude
+		theevent['description'] = event.description
+		theevent['active'] = event.active
+		events.append(theevent)
+	
+	return events
+	
+	
+	

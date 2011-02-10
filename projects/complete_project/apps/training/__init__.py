623a32e5eb42bfe0ebbfa13f1dee664146a6078b
Travis Anderson <tanderson@hisg.org>
2011-02-10 18:18:20 -0500

    update to rpc part in the fourth

--- projects/complete_project/apps/training/__init__.py
+++ projects/complete_project/apps/training/__init__.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from rpc4django import rpcmethod
 from training.models import Event
+from datetime import datetime
 
 @rpcmethod(name='training.add',)
 def add(a,b):
@@ -11,21 +12,39 @@ def add(a,b):
 	
 @rpcmethod(name='training.events',)
 def events():
-	e = Event.objects.all()
+	ue = Event.objects.filter(start_date__gte = datetime.now())
+	pe = Event.objects.filter(start_date__lt = datetime.now())
 	events = []
-	for event in e:
+	uevents = []
+	pevents = []
+	for event in ue:
 		theevent = {}
-		theevent['name'] = event.name
+		# theevent['name'] = event.name
 		theevent['contact'] = event.contact.email
 		theevent['subject'] = event.subject
-		theevent['start_date'] = event.start_date
-		theevent['end_date'] = event.end_date
+		theevent['start_date'] = event.start_date.strftime('%b. %d, %Y')
+		theevent['end_date'] = event.end_date.strftime('%b. %d, %Y')
 		theevent['location'] = event.location
 		theevent['latitude'] = event.latitude
 		theevent['longitude'] = event.longitude
-		theevent['description'] = event.description
+		# theevent['description'] = event.description
 		theevent['active'] = event.active
-		events.append(theevent)
+		uevents.append(theevent)
+	for event in pe:
+		theevent = {}
+		# theevent['name'] = event.name
+		theevent['contact'] = event.contact.email
+		theevent['subject'] = event.subject
+		theevent['start_date'] = event.start_date.strftime('%b. %d, %Y')
+		theevent['end_date'] = event.end_date.strftime('%b. %d, %Y')
+		theevent['location'] = event.location
+		theevent['latitude'] = event.latitude
+		theevent['longitude'] = event.longitude
+		# theevent['description'] = event.description
+		theevent['active'] = event.active
+		pevents.append(theevent)
+	events.append(uevents)
+	events.append(pevents)
 	
 	return events
 	

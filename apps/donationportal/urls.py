5c6b0920bd79456aa781df14037a62c50d2c432a
Travis Anderson <tanderson@hisg.org>
2011-05-19 14:57:28 -0400

    put donation portal into pages

--- apps/donationportal/urls.py
+++ apps/donationportal/urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'donationportal.views.index', name='donation'),
+    # url(r'^$', 'donationportal.views.index', name='donation'),
 	url(r'^login/$', 'donationportal.views.detail', name='donation_login'),
 )
